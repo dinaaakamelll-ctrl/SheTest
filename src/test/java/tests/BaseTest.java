123a53a23345042b46d6134434dfcb3845788e70
ImanElgamal <meman2465@gmail.com>
2026-07-09 05:11:35 +0300

    Modify WebDriver visibility and ChromeOptions
    Changed driver variable from protected to public and added options to prevent password manager prompts.

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,8 +1,7 @@
-package tests;
+package tests; 
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriver; 
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -11,23 +10,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class BaseTest {
-    protected WebDriver driver;
+
+    public WebDriver driver;
 
     @BeforeMethod
     public void setUp() {
-        WebDriverManager.chromedriver().setup();
-
-        // إعدادات بتمنع نافذة "Change your password" اللي بتوقف الكليكات
-        // ده مش بيأثر على كود اللوجن أو التستات، بس بيظبط طريقة فتح المتصفح
         ChromeOptions options = new ChromeOptions();
+        
+        // إعدادات منع ظهور رسائل كلمة السر والـ Autofill عشان التست ما يعلقش
         Map<String, Object> prefs = new HashMap<>();
         prefs.put("credentials_enable_service", false);
         prefs.put("profile.password_manager_enabled", false);
         prefs.put("profile.password_manager_leak_detection", false);
         options.setExperimentalOption("prefs", prefs);
         options.addArguments("--disable-features=PasswordLeakDetection,PasswordManagerOnboarding,AutofillServerCommunication");
+        
+        options.addArguments("--remote-allow-origins=*");
 
-        driver = new ChromeDriver(options);
+        // التشغيل اللوكال المباشر (المستقر والناجح 100%)
+        driver = new ChromeDriver(options); 
+        
         driver.manage().window().maximize();
         driver.get("https://www.saucedemo.com/");
     }
